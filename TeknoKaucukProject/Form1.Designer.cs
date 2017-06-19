2a4f4415b294e8978b679ef779a4bb252f6db568
Betül Metin <betulmetin718@gmail.com>
2017-06-19 04:49:47 +0300

    adding

---
+++ TeknoKaucukProject/Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace TeknoKaucukProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.TextBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Sayılar = new System.Windows.Forms.ListBox();
+            this.TextBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.TextBox8 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(138, 45);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(135, 20);
+            this.TextBox1.TabIndex = 0;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(138, 85);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(135, 20);
+            this.TextBox2.TabIndex = 1;
+            // 
+            // TextBox3
+            // 
+            this.TextBox3.Location = new System.Drawing.Point(136, 129);
+            this.TextBox3.Name = "TextBox3";
+            this.TextBox3.Size = new System.Drawing.Size(135, 20);
+            this.TextBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(376, 85);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(78, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Sonucu Göster";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Location = new System.Drawing.Point(326, 92);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(44, 13);
+            this.Result.TabIndex = 5;
+            this.Result.Text = "Sonuç :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(61, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Saymaya Başla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Sayılar
+            // 
+            this.Sayılar.FormattingEnabled = true;
+            this.Sayılar.Location = new System.Drawing.Point(214, 18);
+            this.Sayılar.Name = "Sayılar";
+            this.Sayılar.Size = new System.Drawing.Size(176, 446);
+            this.Sayılar.TabIndex = 7;
+            // 
+            // TextBox5
+            // 
+            this.TextBox5.Location = new System.Drawing.Point(323, 37);
+            this.TextBox5.MaxLength = 2;
+            this.TextBox5.Name = "TextBox5";
+            this.TextBox5.Size = new System.Drawing.Size(129, 20);
+            this.TextBox5.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(495, 35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Çarpım tablosu oluştur";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(135, 32);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Dosya Seçiniz";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // TextBox8
+            // 
+            this.TextBox8.Location = new System.Drawing.Point(284, 66);
+            this.TextBox8.Multiline = true;
+            this.TextBox8.Name = "TextBox8";
+            this.TextBox8.Size = new System.Drawing.Size(262, 295);
+            this.TextBox8.TabIndex = 13;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(115, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(770, 386);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(392, 98);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(157, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Fibonacci Sayısını Gör";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(391, 53);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(158, 20);
+            this.textBox6.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(452, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 17;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(12, 5);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1009, 910);
+            this.tabControl1.TabIndex = 18;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.TextBox1);
+            this.tabPage1.Controls.Add(this.TextBox2);
+            this.tabPage1.Controls.Add(this.TextBox3);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.Result);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1001, 884);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "1. İşlem";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Üçüncü sayı :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "İkinci sayı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Birinci sayı :";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.Sayılar);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1001, 884);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "2. İşlem";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.TextBox5);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.dataGridView1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1001, 884);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "3. İşlem";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(177, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "1-15 arası bir tamsayı giriniz :";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.label7);
+            this.tabPage4.Controls.Add(this.button4);
+            this.tabPage4.Controls.Add(this.TextBox8);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1001, 884);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "4. İşlem";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(281, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(265, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Metin dosyasındaki sayıların büyükten küçüğe sıralanışı";
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.label6);
+            this.tabPage5.Controls.Add(this.textBox6);
+            this.tabPage5.Controls.Add(this.label1);
+            this.tabPage5.Controls.Add(this.button5);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(1001, 884);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "5. İşlem";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(295, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Bir sayı giriniz :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1020, 541);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.TextBox TextBox2;
+        private System.Windows.Forms.TextBox TextBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox Sayılar;
+        private System.Windows.Forms.TextBox TextBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox TextBox8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ TeknoKaucukProject/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Collections;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TeknoKaucukProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int firstcount = Convert.ToInt32(TextBox1.Text); //birinci textbox değeri alınır
+            int secondcount = Convert.ToInt32(TextBox2.Text); //ikinci textbox değeri alınır
+            int thirdcount = Convert.ToInt32(TextBox3.Text); //üçüncü textbox değeri alınır
+            int result = (firstcount + secondcount)*thirdcount;
+            // birinci ve ikinci sayı toplanarak üçüncü sayı ile çarpılır
+            textBox4.Text = Convert.ToString(result); //sonuç dördüncü textbox a yazılır
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 200; i++) // 1'den 200'e kadar sayılar yazılır
+            {
+                if (i%3 == 0 && i%5 == 0) //Sayının hem 3'ün hem de 5'in katı olduğu durum
+                {
+                    if (i <= 100) //eğer 100'den küçükse sayı yerine ZigZag yazılsın
+                    {
+                        Sayılar.Items.Add("ZigZag");
+                    }
+                    else //eğer 100'den büyükse sayı yerine ZagZig yazılsın
+                    {
+                        Sayılar.Items.Add("ZagZig");
+                    }
+                }
+
+                else if (i%3 == 0) //Sayı sadece 3'ün katıysa Zig yazılsın
+                {
+                    Sayılar.Items.Add("Zig");
+                }
+
+
+                else if (i%5 == 0) //Sayı sadece 5'in katıysa Zag yazılsın
+                {
+                    Sayılar.Items.Add("Zag");
+                }
+                else
+                {
+                    Sayılar.Items.Add(i); //3'ün veya 5'in katı olmadığı durumlarda sayının kendisi yazılsın
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int count = Convert.ToInt32(TextBox5.Text);
+
+            if (count >= 16) //Textboxa 1-15 arasında değer giriniz uyarısının verilmesi
+            {
+                MessageBox.Show("Lütfen 1-15 arasında bir tam sayı giriniz!..");
+            }
+            else
+            {
+                {
+
+                    DataTable dataTable = new DataTable();
+
+                    for (int i = 0; i <= count; i++) //Çarpım tablosunun satır ve sütunlarının oluşturulması
+                    {
+                        if (i == 0)
+                        {
+                            dataTable.Columns.Add("X");
+                        }
+                        else
+                        {
+                            dataTable.Columns.Add(i.ToString());
+                            dataTable.Rows.Add(i);
+                        }
+
+
+                    }
+                    for (int i = 1; i <= count; i++) //Çarpım işlemlerinin yapılması
+                    {
+                        for (int j = 1; j <= count; j++)
+                        {
+                            dataTable.Rows[i - 1][j] = i*j;
+                        }
+                    }
+
+                    dataGridView1.DataSource = dataTable;
+                    for (int i = 0; i <= count; i++)
+                    {
+                        dataGridView1.Columns[i].Width = 50;
+                    }
+
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Metin Dosyası |*.txt";
+            file.Title = "Metin Dosyasını Seçiniz..";
+            file.ShowDialog(); //Metin dosyasının seçilmesi
+            string filePath = file.FileName;
+            StreamReader st = File.OpenText(filePath);
+            List<double> numbers = new List<double>(); // rakamların tutulacagi double tipinde listenin tanımlanması
+
+            while (!st.EndOfStream) // okunan metin dosyasında satır bazlı gezinmek icin
+            {
+                IEnumerable<string> array = Regex.Split(st.ReadLine(), @"\s+").Where(s => s != string.Empty);
+                // sayılar arasında bir karakterden fazla bosluk olabilir bu durumdan kurtulmak icin string.Empty ile kontrol edilmesi
+
+                foreach (string t in array) // okunan satırdaki rakamları double'a convert islemi
+                {
+                    try
+                        // istenmeyen karakterler gelmiş olabilir bunun icin try- catch blogu(burada kullanıcıya uyari verilebilir)
+                    {
+                        numbers.Add(Convert.ToDouble((t)));
+                    }
+                    catch (Exception)
+                    {
+                        // ignored
+                    }
+                }
+
+            }
+            numbers.Sort(); // rakamların sıralama islemi                
+
+            foreach (double t in numbers) // rakamları ekranda listeleme islemi
+            {
+                TextBox8.Text += Convert.ToString(t, CultureInfo.InvariantCulture) + " ";
+                TextBox8.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            int number = Convert.ToInt32(textBox6.Text);
+            int newFibonacciNumber = 1;
+            int oldFibonacciNumber = 0;
+            int temp;
+            for (int i = 0; i < number - 2; i++)  //Fibonaccı dizisi her sayının kendinden öncekiyle toplanması sonucu oluşan sayı dizisidir
+            {
+                temp = newFibonacciNumber;      //for döngüsünde sayıyı bulmak için gerekn işlem yapılmıştır
+                newFibonacciNumber += oldFibonacciNumber;
+                oldFibonacciNumber = temp;
+
+            }
+            label1.Text = newFibonacciNumber.ToString(); //bulunan sayı ekrana yazdırılır
+        }
+
+      
+    }
+}
+        
+
+      
+
